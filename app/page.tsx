61cdfc6f07bbdd07c8b28808725cfadea2d199e7
GJ Bos <bosmeel@gmail.com>
2026-03-19 20:46:05 +0100

    Add Timbo logo and favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,27 @@
 "use client";
 
+import Image from "next/image";
 export const dynamic = "force-dynamic";
 
 export default function Home() {
-
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center bg-white">
+    <main className="min-h-screen flex flex-col items-center justify-center bg-white px-6">
 
-      {/* 👇 disclaimer */}
+      <Image
+        src="/logo-timbo.svg"
+        alt="Timbo"
+        width={160}
+        height={40}
+        className="mb-6 h-auto"
+        priority
+      />
+
       <div className="text-center text-sm text-gray-500 mb-4 max-w-xs">
         This timer works best on modern devices.
         <br />
         Older iPads (iOS 12 and below) are not supported.
       </div>
 
-      {/* 👇 knop */}
       <a
         href="/timer"
         className="px-6 py-3 rounded-xl bg-black text-white"
