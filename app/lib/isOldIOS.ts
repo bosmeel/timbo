741401c779a57bb33f1320203399ad662b13bc73
GJ Bos <bosmeel@gmail.com>
2026-03-19 19:08:33 +0100

    update

--- app/lib/isOldIOS.ts
+++ app/lib/isOldIOS.ts
@@ -3,17 +3,6 @@ export function isOldIOS() {
 
   const ua = navigator.userAgent;
 
-  const isIOS = /iPad|iPhone|iPod/.test(ua);
-  const isOldVersion = /OS (9|10|11|12)_/.test(ua);
-
-  return isIOS && isOldVersion;
-}
-
-export function isOldIOS() {
-  if (typeof navigator === "undefined") return false;
-
-  const ua = navigator.userAgent;
-
   const isIOS =
     /iPad|iPhone|iPod/.test(ua) ||
     (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1);
