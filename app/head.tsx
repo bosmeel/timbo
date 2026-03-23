1170afed3f0c7c996a197c394ae77bae25885ecd
GJ Bos <bosmeel@gmail.com>
2026-03-23 11:17:17 +0100

    update

--- app/head.tsx
+++ app/head.tsx
@@ -1,11 +1,17 @@
 export default function Head() {
   return (
     <>
+      {/* AdSense */}
       <script
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5866549959429332"
         crossOrigin="anonymous"
       ></script>
+
+      {/* Favicon fix */}
+      <link rel="icon" href="/favicon.svg" />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="apple-touch-icon" href="/favicon.svg" />
     </>
   );
 }
