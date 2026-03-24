e8e5bebb2e04fe0252c0210bcbe102cf88560522
GJ Bos <bosmeel@gmail.com>
2026-03-24 07:37:07 +0100

    update

--- app/timer/page.tsx
+++ app/timer/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+export const dynamic = "force-dynamic";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useRef, useState, Suspense } from "react";
